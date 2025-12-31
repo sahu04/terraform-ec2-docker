6199b02c7afe95d484aa162d2beeabf20c6bb1d1
sahu04 <105695204+sahu04@users.noreply.github.com>
2025-12-31 16:52:04 +0530

    Update app.js

--- app/app.js
+++ app/app.js
@@ -1,8 +1,31 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello from Docker + Terraform on AWS EC2 🚀");
+  res.writeHead(200, { "Content-Type": "text/html" });
+
+  res.end(`
+    <html>
+      <head>
+        <title>Docker + Terraform</title>
+      </head>
+      <body style="
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        background-color: #f4f6f8;
+      ">
+        <h1 style="
+          color: #1e90ff;
+          font-size: 48px;
+          font-weight: bold;
+          font-family: Arial, Helvetica, sans-serif;
+        ">
+           Hello from Docker + Terraform on AWS EC2 
+        </h1>
+      </body>
+    </html>
+  `);
 });
 
 server.listen(3000, () => {
